3bf972bd65e92af16232f8e30fd2a207095bf186
mg699 <mg699@kent.ac.uk>
2021-04-12 14:31:31 +0100

    Changed project structure

--- src/php/twig_init.php
+++ src/php/twig_init.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 function init() {
-	$loader = new FilesystemLoader(__DIR__ . '/templates');
+	$loader = new FilesystemLoader(__DIR__ . '/../templates');
 	$twig = new Environment($loader);
 	
 	return $twig;
