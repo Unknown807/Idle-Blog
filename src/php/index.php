303d0897838abd81bbb5476261755ee2e8b778b8
Unknown807 <milvovangveric@gmail.com>
2021-04-14 18:41:11 +0100

    Added base template

--- src/php/index.php
+++ src/php/index.php
@@ -2,6 +2,8 @@
 	require_once 'twig_init.php';
 	$twig = init();
 
-	echo $twig->render("index.html.twig",
-		["first_page_path" => "first.php"]);
+	echo $twig->render("index.html.twig");
+
+	//echo $twig->render("index.html.twig",
+	//	["first_page_path" => "first.php"]);
 ?>
